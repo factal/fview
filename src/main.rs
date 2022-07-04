d9dd979197d33840530c4eef749c9c2e786dde72
factal <joji0425jp@gmail.com>
2022-07-04 13:41:37 +0900

    feature: reading file

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,29 @@
+use std::fs::File;
 use std::io::prelude::*;
+use std::env;
 
 const BYTES_PER_LINE: usize = 16;
-const INPUT: &'static [u8] = br#"
 fn main() {
-  println!("Hello, world!");
-}"#;
+  let arg1 = env::args().nth(1);
 
-fn main() -> std::io::Result<()> {
-  let mut buffer: Vec<u8> = vec!();
-  INPUT.read_to_end(&mut buffer)?;
+  let fname = arg1.expect("usage: fview <FILENAME>");
 
-  let mut position_in_input = 0;
-  for line in buffer.chunks(BYTES_PER_LINE) {
-    print!("[0x{:08x}] ", position_in_input);
-    for byte in line {
-      print!("{:02x} ", byte);
+  let mut f = File::open(&fname).expect("unable to open file.");
+  let mut pos = 0;
+
+  let mut buffer = [0; BYTES_PER_LINE];
+  
+  while let Ok(_) = f.read_exact(&mut buffer) {
+    println!("[0x{:08x}]", pos);
+    for byte in &buffer {
+      match *byte {
+        0x00 => print!(". "),
+        0xff => print!("## "),
+        _ => print!("{:02x} ", byte)
+      }
     }
-    println!(); // output line break
-    position_in_input += BYTES_PER_LINE;
-  }
 
-  Ok(())
+    println!("");
+    pos += BYTES_PER_LINE;
+  }
 }
